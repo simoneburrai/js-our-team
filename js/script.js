bc245f25d4e564eed4646d3b74b222dc7001da0d
simoneburrai <burrai.s98@gmail.com>
2025-03-10 18:47:15 +0100

    feat: created card element on javascript and given reponsivity to layout

--- js/script.js
+++ js/script.js
@@ -37,4 +37,46 @@ const teamMembers = [
   }
 ];
 
-console.log(teamMembers)
+console.log(teamMembers);
+const cardContainerElement = document.getElementById("card-container");
+console.log(cardContainerElement);
+
+
+cardContainerElement.innerHTML = "";
+for(let i=0; i < teamMembers.length; i++){
+  currentMember = teamMembers[i];
+  const newCardElement = cardElementCreating(currentMember);
+  console.log(newCardElement);
+    cardContainerElement.innerHTML += newCardElement;
+}
+
+
+console.log({cardContainerElement});
+
+
+
+
+function cardElementCreating(obj){
+  const name = obj.name;
+  console.log(name)
+  const role = obj.role;
+  console.log(role)
+  const img = obj.img;
+  console.log(img)
+  const email = obj.email;
+  console.log(email);
+
+  const cardElement = `
+  <div class="card">
+    <div class="img-container">
+      <img id="image" src="${img}" alt="${name}">
+    </div>
+    <div class="card-info">
+        <div id="name">${name}</div>
+        <div id="role">${role}</div>
+        <a href="" id="email">${email}</a>
+    </div>
+  </div>
+  `          
+return cardElement;
+}
